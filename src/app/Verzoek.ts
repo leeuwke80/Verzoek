2ffb85cb0c0728656a4d1f12419239ccff7b9344
leeuwke80 <leeuwke@gmail.com>
2023-08-16 01:25:42 +0200

    third commit

---
+++ src/app/Verzoek.ts
@@ -0,0 +1,7 @@
+export class Verzoek {
+  constructor(
+    public aanvrager:string,
+    public artiest:string,
+    public nummer:string
+  ){}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Verzoek } from './Verzoek';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Verzoek';
+  ngOnInit(): void {
+    this.lstVerzoek.push(new Verzoek("Aanvrager","Artiest","Nummer"))
+  }
+  titel = 'Verzoek';
+  lstVerzoek:Array<Verzoek>=[];
+  ngAanvrager:string="";
+  ngArtiest:string="";
+  ngNummer:string="";
+  aanvragen(){
+    this.lstVerzoek.push(new Verzoek(this.ngAanvrager,this.ngArtiest,this.ngNummer));
+    this.clearfields();
+  }
+  clearfields(){
+    this.ngAanvrager="",this.ngArtiest="",this.ngNummer="";
+  }
 }
